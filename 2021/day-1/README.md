105b4dbc665ad80fcd6460b5e6ff02eb0728d9fb
Casper Aangeenbrug <cas.aangeenbrug@gmail.com>
2022-09-17 09:21:40 +0200

    Add day 1 solution

---
+++ 2021/day-1/README.md
@@ -0,0 +1,2 @@
+This is aweful code.
+I am still learning about how to do the error handling in a correct manner..
--- 2021/day-1/src/main.rs
+++ 2021/day-1/src/main.rs
@@ -1,3 +1,43 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("input.txt");
+
+    match file {
+        Ok(found_file) => {
+            let reader = BufReader::new(found_file);
+
+            let mut last_value: Option<i32> = None;
+            let mut count: i32 = 0;
+            for line in reader.lines() {
+                match line {
+                    Ok(line_str) => {
+                        let current_value_result = line_str.parse::<i32>();
+                        match current_value_result {
+                            Ok(current_value) => {
+                                match last_value {
+                                    Some(last) => {
+                                        if current_value > last {
+                                            count += 1;
+                                        }
+                                    }
+                                    None => (),
+                                }
+
+                                last_value = Some(current_value);
+                            }
+                            Err(e) => panic!("{}", e),
+                        }
+                    }
+                    Err(e) => panic!("{}", e),
+                }
+            }
+
+            println!("{}", count);
+        }
+        Err(e) => panic!("{}", e),
+    }
 }
